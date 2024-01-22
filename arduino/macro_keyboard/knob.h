383a8a54e3e71595bc7b06eea26264f78b26f3e9
Guilherme Beidaki Peres <guibperes@protonmail.com>
2024-01-22 19:25:10 -0300

    moved knob definitions to header file

---
+++ arduino/macro_keyboard/knob.h
@@ -0,0 +1,9 @@
+#ifndef knob_h
+#define knob_h
+
+#define KNOB_TYPE 1
+#define KNOB_QUANTITY 1
+#define KNOB_MILLIS_DELAY 200
+#define KNOB_READ_DELAY 10
+
+#endif
--- arduino/macro_keyboard/macro_keyboard.ino
+++ arduino/macro_keyboard/macro_keyboard.ino
@@ -1,9 +1,5 @@
 #include "button.h"
-
-#define KNOB_TYPE 1
-#define KNOB_QUANTITY 1
-#define KNOB_MILLIS_DELAY 200
-#define KNOB_READ_DELAY 10
+#include "knob.h"
 
 static const byte analog_pins[] = { A0, A1, A2, A3, A4, A5 };
 
