c0be0aafb589b11cfcb05ef219496ee9a08b8a1f
Guilherme Beidaki Peres <guibperes@protonmail.com>
2024-01-22 19:23:02 -0300

    moved button definitions to header file

---
+++ arduino/macro_keyboard/button.h
@@ -0,0 +1,9 @@
+#ifndef button_h
+#define button_h
+
+#define BUTTON_TYPE 0
+#define BUTTON_FIRST_PIN 7
+#define BUTTON_QUANTITY 5
+#define BUTTON_PRESS_TIME 200
+
+#endif
--- arduino/macro_keyboard/macro_keyboard.ino
+++ arduino/macro_keyboard/macro_keyboard.ino
@@ -1,7 +1,4 @@
-#define BUTTON_TYPE 0
-#define BUTTON_FIRST_PIN 7
-#define BUTTON_QUANTITY 5
-#define BUTTON_PRESS_TIME 200
+#include "button.h"
 
 #define KNOB_TYPE 1
 #define KNOB_QUANTITY 1
