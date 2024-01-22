6a9f7c13baca58180f91b803f7d57530aa51fa1a
Guilherme Beidaki Peres <guibperes@protonmail.com>
2024-01-22 00:32:17 -0300

    repo: init

---
+++ arduino/macro_keyboard.ino
@@ -0,0 +1,38 @@
+#define BUTTON_FIRST_PIN 7
+#define BUTTON_QUANTITY 5
+#define BUTTON_PRESS_TIME 200
+
+struct button {
+  byte pin;
+  long last_press;
+};
+
+button buttons[BUTTON_QUANTITY];
+
+bool check_button_last_press(button btn) {
+  return millis() - btn.last_press >= BUTTON_PRESS_TIME;
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  for (byte i = 0; i < BUTTON_QUANTITY; i++) {
+    byte pin = BUTTON_FIRST_PIN + i;
+
+    buttons[i].pin = pin;
+    buttons[i].last_press = BUTTON_PRESS_TIME;
+
+    pinMode(pin, INPUT_PULLUP);
+  }
+}
+
+void loop() {
+  for (byte i = 0; i < BUTTON_QUANTITY; i++) {
+    bool button_state = digitalRead(buttons[i].pin);
+
+    if (check_button_last_press(buttons[i]) && button_state == LOW) {
+      Serial.write(buttons[i].pin);
+      buttons[i].last_press = millis();
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { exec } from "node:child_process";
+import { SerialPort } from "serialport";
+
+const executeProcess = (command, { unref } = { unref: true }) => {
+  console.log(`Executing command: ${command}`);
+  const subprocess = exec(command);
+
+  if (unref) subprocess.unref();
+};
+
+const ACTIONS = {
+  7: () => executeProcess('echo "Testando" > teste.txt'),
+  8: () => executeProcess("librewolf"),
+  9: () => executeProcess("playerctl previous"),
+  10: () => executeProcess("playerctl play-pause"),
+  11: () => executeProcess("playerctl next"),
+};
+
+const port = new SerialPort({ path: "/dev/ttyACM0", baudRate: 9600 });
+
+port.on("data", (data) => {
+  const actionNumber = Number.parseInt(Buffer.from(data).toString("hex"), 16);
+  ACTIONS[actionNumber]();
+});
